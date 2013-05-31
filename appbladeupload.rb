056d652670c099e7c896a5154584ef1c58337c08
Alex Rouse <alex.rouse@raizlabs.com>
2013-05-31 09:02:27 -0400

    Fixing the cURL statement and updating the required parameters

--- appbladeupload.rb
+++ appbladeupload.rb
@@ -8,29 +8,33 @@ module ABUpload
 
 		### Functions ###
 
-		def upload_to_appblade(build_path, project_uuid, ci_token, ipa_path, dsym_name, version_string, release_track)
-			
-			output_path = FILE.realpath(build_path)
-			full_ipa_path = FILE.join(output_path, ipa_path + ".ipa")
-			dsym_path = FILE.realpath(dsym_name)
-			puts "INSIDE THE APPBLADEUPLOADER!"
+		def upload_to_appblade(build_path, ci_token, ipa_path, dsym_name, version_string, release_track)
+			puts "Build Path: ",build_path
+            puts "CI Token: ",ci_token
+            puts "IPA Path: ",ipa_path
+            puts "dSYM Path: ",dsym_name
+            puts "Version String: ",version_string
+            puts "Release Track: ",release_track
+            
+			output_path = File.realpath(build_path)
+			full_ipa_path = File.join(output_path, ipa_path)
+            dsym_path = File.join(output_path, dsym_name)
 
-    		puts 'curl -# -H "Accept: application/json"  -H "Authorization: Bearer #{ci_token}" -F "version[bundle]=@#{full_ipa_path}" -F "version[dsym]=@#{dsym_path}" -F "version[commit_id]=#{version_string}" -F "version[version_string]=#{version_string}" -F "version[release_track_list]=#{release_track}" https://appblade.com/api/projects/#{project_uuid}/versions'
-    		raise "Hey We did something wrong Return Code: #{$?}" if ($! != 0)
-    	#	  curl -# -H "Authorization: Bearer $APPBLADE_JENKINS_OAUTH_TOKEN" -F "version[bundle]=@$IPA_PATH" -F "version[dsym]=@$DSYM_NAME" -F "version[commit_id]#=$GIT_TAG" -F "version[version_string]=$GIT_TAG" -F "version[changelog]=$RELEASE_CHANGELOG" -F "version[release_track_list]=$RELEASE_TRACK" \
-  		#https://appblade.com/api/3/versions
+    		puts `curl -# -H "Accept: application/json"  -H "Authorization: Bearer #{ci_token}" -F "version[bundle]=@#{full_ipa_path}" -F "version[dsym]=@#{dsym_path}" -F "version[commit_id]=#{version_string}" -F "version[version_string]=#{version_string}" -F "version[release_track_list]=#{release_track}" https://appblade.com/api/3/versions`
+            
+    		raise "Error uploading application: #{$?}" if ($! != 0)
 		end
 	end
 
 end
 
-#What is required:
-# BuildPath, Project UUID, CIToken, ipa_path, 
+# What is required:
+# BuildPath, CIToken, ipa_path, 
 
 class CLI < Clive
-	opt :dsym, :dsym_name, arg: '<dsym_name>'
+	opt :d, :dsym_name, arg: '<dsym_name>'
 	opt :v, :version_string, arg: '<version_string>'
-	opt :rt, :releast_track, arg: '<release_track>'
+	opt :r, :release_track, arg: '<release_track>'
 end
 
 result = CLI.run
@@ -38,7 +42,8 @@ result = CLI.run
 uploader = ABUpload::Uploader.new()
 
 begin
-	uploader.upload_to_appblade(result.args[0], result.args[1], result.args[2], result.args[3], result[:dsym_name], result[:version_string], result[:release_track])
+    puts "Begining the upload to Appblade Proccess"
+	uploader.upload_to_appblade(result.args[0], result.args[1], result.args[2], result[:dsym_name], result[:version_string], result[:release_track])
 rescue Exception
 	puts "UPLOAD FAILED: #{$!}"
 	return -1
